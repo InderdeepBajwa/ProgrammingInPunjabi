fe3937901b2d497d2bd9424c2749ece8dbb17dab
Inderdeep Singh Bajwa <InderdeepBajwa@users.noreply.github.com>
2018-05-02 02:27:56 -0400

    Create CinCout.cpp

---
+++ C Plus Plus/Session1/CinCout.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	double myNumber,
+		nextNumber;
+
+	cout << "Enter 1st number: ";
+	cin >> myNumber;
+	cout << "Enter 2nd number: ";
+	cin >> nextNumber;
+
+	double teeja;
+
+	teeja = myNumber / nextNumber;
+
+	cout << teeja << endl;
+
+
+
+	system("pause");
+}
