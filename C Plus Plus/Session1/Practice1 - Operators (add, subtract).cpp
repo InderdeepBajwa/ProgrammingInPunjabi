2f9d59515ed07b1af0bbf8cebe50526481b39d9b
Inderdeep Singh Bajwa <InderdeepBajwa@users.noreply.github.com>
2018-05-23 15:58:23 -0400

    Create Practice1 - Operators (add, subtract).cpp

---
+++ C Plus Plus/Session1/Practice1 - Operators (add, subtract).cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int x = (9 + 1) * (2 - 6) / 4;
+	//		(9 + 1) * (2 - 6) / 4
+
+	// BODMAS
+
+	/*
+		8 + 1 - 2: 
+			8+1 = 9; 
+			9 - 2 = 7;
+
+			8 - 2 = 6;
+			6 + 1 = 7;
+
+	*/
+	
+	cout << x;
+
+
+	cin.get();
+}
